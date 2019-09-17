a6f4685612f905d0ebc61b061a2ae55a3c1c0295
yekeren <yekeren.cn@gmail.com>
2019-09-17 12:32:19 -0400

    recent changes

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ set -x
 
 protoc protos/*.proto --python_out=. || exit -1
 
+if [ ! -d "tensorflow_models" ]; then
+  git clone "https://github.com/tensorflow/models.git" "tensorflow_models"
+  ln -s "tensorflow_models/research/object_detection" .
+fi
+
 exit 0
